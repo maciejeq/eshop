d1e7921232cfe3faacac4f507ee3a8ecb944453c
Maciek <maciejpietranik@gmail.com>
2017-11-24 11:32:58 +0100

    Added custom views for payments in admin site

--- orders/admin.py
+++ orders/admin.py
@@ -1,6 +1,35 @@
+import csv
+import datetime
 from django.contrib import admin
 from .models import Order, OrderItem
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse
 
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename={}.csv'.format(opts.verbose_name)
+    writer = csv.writer(response)
+
+    fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
+    # write the header row
+    writer.writerow([field.verbose_name for field in fields])
+    # write data rows
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+export_to_csv.short_description = 'Export to CSV'
+
+def order_detail(obj):
+    return '<a href="{}">View</a>'.format(reverse('orders:admin_order_detail',
+                                                  args=[obj.id]))
+order_detail.allow_tags = True
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
@@ -8,8 +37,10 @@ class OrderItemInline(admin.TabularInline):
 
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'paid', 'created', 'updated']
+    list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'paid', 'created', 'updated', order_detail]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
+    actions = [export_to_csv]
+
 
 admin.site.register(Order, OrderAdmin)
--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.core.urlresolvers import reverse
-from .models import OrderItem
+from django.contrib.admin.views.decorators import staff_member_required
+from .models import Order, OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
 from .tasks import order_created
@@ -27,3 +28,19 @@ def order_create(request):
         form = OrderCreateForm()
     return render(request, 'orders/order/create.html', {'cart': cart,
                                                         'form': form})
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request, 'admin/orders/order/detail.html', {'order': order})
+
+
+@staff_member_required
+def admin_order_pdf(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    html = render_to_string('orders/order/pdf.html', {'order': order})
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'filename="order_{}.pdf"'.format(order.id)
+    weasyprint.HTML(string=html).write_pdf(response,
+                                           stylesheets=[weasyprint.CSS(settings.STATIC_ROOT + 'css/pdf.css')])
+    return response
