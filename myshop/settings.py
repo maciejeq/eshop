8a4b89b57f28ad237d9318f043429349445763ba
Maciek <maciek@MacBook-Pro-Maciek.local>
2017-11-28 16:15:33 +0100

    started introducing translation / language change

--- myshop/settings.py
+++ myshop/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = (
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -93,8 +94,18 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
 
+from django.utils.translation import gettext_lazy as
+
+LANGUAGES = (
+    ('en', 'English'),
+    ('es', 'Spanish'),
+
+)
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'locale/'),
+)
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
